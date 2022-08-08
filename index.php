5cbb1c03d27a870a3c8fbd157fca2e20aacf21a9
sowjanya chepuri <swjnambati@gmail.com>
2022-08-08 11:41:32 +0530

    init commitfirts change

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Untitled Document</title>
+</head>
+<?php
+$n=5;
+for($i=1;$i<=$n;$i++)
+{
+//echo "*"." ";
+	echo " ";
+	for($j=$i;$j<=$n-1;$j++)
+	
+	{
+		echo " "."*";
+	}
+	
+	echo "</br>";
+}
+
+
+
+?>
+
+
+
+
+
+<body>
+</body>
+</html>
